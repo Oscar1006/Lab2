b66cbfeaad68eab01b9fbe7f5ee3108461965119
Oscar1006 <oscar.suarez1@correo.icesi.edu.co>
2020-04-11 00:26:40 -0500

    Image and 2 buttons added

--- src/ui/MainUI.java
+++ src/ui/MainUI.java
@@ -6,33 +6,59 @@ import java.io.*;
 import java.awt.*;
 import java.awt.event.*;
 
-
 class Window extends JFrame {
+	
 	public Window() {
-		setSize(1000,700);
+		setSize(800,700);
 		setTitle("The Pirate");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		setResizable(false);
 		
-
-		Text title = new Text();
-		add(title);
-		
-		BoatImage ship = new BoatImage();
-		add(ship); 
-
-		Buttons actions = new Buttons();
-		add(actions);
-		
+		Sheet principal = new Sheet();
+		add(principal);
 	}
 }
+class Sheet extends JPanel {
+	JButton upload;
+	JButton download;
+
+	private Image boat;
+
+
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		
+		setLayout(null);
+
+		upload = new JButton("Upload Boat");
+		download = new JButton("Download Boat");
+
+		upload.setBounds(550, 40, 150, 40);
+		download.setBounds(550, 100, 150, 40);
+
+		add(upload);
+		add(download);
+
+
+		File b = new File("src/image/boat.png");
+		try{
+			boat = ImageIO.read(b);
+		}
+		catch(IOException e){
+			System.out.println("Image not found");
+		}
+		g.drawImage(boat, 10, 40, null);
+	}
+}
+/*
 class Text extends JPanel {
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.drawString("Welcome to Morgan's Boat", 50, 20);
 	}
 }
+
 class BoatImage extends JPanel{
 	private Image boat;
 
@@ -49,20 +75,6 @@ class BoatImage extends JPanel{
 	}
 }
 
-class Buttons extends JPanel implements ActionListener {
-	JButton upload = new JButton("Upload Boat");
-	JButton download = new JButton("Download Boat");
-	public Buttons(){
-		add(upload);
-		upload.addActionListener(this);
-		add(download);
-		
-	}
-	public void actionPerformed(ActionEvent e){
-
-	}
-}
-
 class WindowActions implements WindowListener{
 	public void windowActivated(WindowEvent e){}
 	public void windowClosed(WindowEvent e){}
@@ -73,6 +85,8 @@ class WindowActions implements WindowListener{
 	public void windowOpened(WindowEvent e){}
 
 }
+*/
+
 
 public class MainUI {	
 	public static void main(String[] args) {
@@ -80,4 +94,3 @@ public class MainUI {
 		inter.setVisible(true);
 	}
 }
-
